d1a9949a4df50b35cb9b7109a0371c23f6dc5307
Sumit Sarkar <91620203+Sumitswappy@users.noreply.github.com>
2025-03-30 15:37:17 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ app.use('/order', orderRouter);
 // Add more routes and middleware as needed for future functionalities
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
